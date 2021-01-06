22d084e01c591a1400ede65205d8e4b751c7b82c
mockingjay07 <bouchra0805@gmail.com>
2021-01-06 23:01:41 +0100

    parent_id added

--- Scrapping/preprocessing/parent_id_add.py
+++ Scrapping/preprocessing/parent_id_add.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
+dtypes = {'hs_code': 'str', 'description': 'str','parent': 'str'}
+df = pd.read_csv('/home/bouchra/PycharmProjects/ECP_scraper/Scrapping/data/new_australia.csv', dtype=dtypes)
 
-df = pd.read_csv('/home/bouchra/PycharmProjects/ECP_scraper/Scrapping/data/new_australia.csv', sep=',')
+# print(df.loc[df['hs_code'] == '01'].id.item())
+
+parent_ids = []
+parents = df['parent'].values
+
+for i in parents:
+    parent_ids.append(df.loc[df['hs_code'] == i].id.item())
+print(parent_ids)
+
+df['parent_id'] = parent_ids
+del df["parent"]
 
 
-df.to_csv('/home/bouchra/PycharmProjects/ECP_scraper/Scrapping/data/new_australia.csv')
+df.to_csv('/home/bouchra/PycharmProjects/ECP_scraper/Scrapping/data/australia_final.csv',index = None)
